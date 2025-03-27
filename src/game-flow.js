f101f0e96c09fc69db80e9df0dd8cca902483c39
Hayam <110453871+Hayam999@users.noreply.github.com.>
2025-03-27 03:49:32 +0200

    add: ui to start the game

---
+++ src/game-flow.js
@@ -0,0 +1,9 @@
+function controlGameFlow(positions) {
+  // TODO create gameBoard for the user using given positions
+  // TODO create computer's gameBoard
+  // TODO display the 2 gameBoards
+  // TODO control the game flow
+  console.log("start playing");
+}
+
+export { controlGameFlow };
---
+++ src/place-ships.js
@@ -0,0 +1,9 @@
+async function getShipsPoses() {
+  // TODO design ui for the user to place their ships;
+  return new Promise((resolve) => {
+    // RETURN all positions that user have chose
+    resolve([]);
+  });
+}
+
+export { getShipsPoses };
